db0cb0cd5e875125875a5fcb661d47457b61f865
elkinssm <elkinsilvamanrique@gmail.com>
2022-08-30 18:23:21 -0500

    Finhis app

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,24 +1,15 @@
 import React from 'react'
 import '../index.css'
 
-// El componente Cabera no tiene componentes hijos.
-// ESTADO: Cabecera no tiene estado.
-// MÉTODOS: Cabecera no tiene métodos.
-// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
-// Maqueta de Cabecera:
-//    h1
-//    p > span     (el span mostrará la cantidad recibida por props)
+export default function Cabecera({ totalItems }) {
 
-export default function Cabecera() {
-  
   return (
     <header>
-        {/* maquetar Cabecera aquí */}
-        <h1>Carrito de compras</h1>
-        <p>Cantidad de productos
-        <span>cantidad productos por props</span>
-        </p>
-        
+      <h1>Carrito de compras</h1>
+      <p>Cantidad de productos &nbsp;
+        <span>{totalItems}</span>
+      </p>
+
     </header>
   )
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,16 @@
 import { useEffect, useState } from "react";
 import "../index.css";
-// El componente Item no tiene componentes hijos.
-// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
-// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
-// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
-// Maqueta de Item:
-//    h3
-//    p
-//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
-//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
 
-export default function Item({ data }) {
+export default function Item({ data, totalItems, setTotalItems }) {
+  
   const [stock, setStock] = useState([]);
-  // const handletStock = () => { };
 
   const buyProduct = (product) => {
     setStock(
       stock.map((item) => {
         if (item.id === product.id) {
           item.stock = item.stock - 1;
+          setTotalItems(totalItems + 1);
         }
         return item;
       })
@@ -26,24 +18,27 @@ export default function Item({ data }) {
   };
 
   useEffect(() => {
-    setStock(data)
-  }, [])
-
+    setStock(data);
+  }, []);
 
   return (
     <>
       {stock.map((product) => (
-        <div className="producto">
-          {/* maquetar Item aquí */}
+        <div className="producto" key={product.id}>
           <h3>{product.producto.nombre}</h3>
           <p>{product.producto.descripcion}</p>
           <h5>
             En stock: {product.stock > 0 ? product.stock : <span>agotado</span>}
           </h5>
-          <button onClick={() => buyProduct(product)} disabled={product.stock < 1}> {product.stock > 0 ? "COMPRAR" : "SIN STOCK"}</button>
+          <button
+            onClick={() => buyProduct(product)}
+            disabled={product.stock < 1}
+          >
+            {" "}
+            {product.stock > 0 ? "COMPRAR" : "SIN STOCK"}
+          </button>
         </div>
       ))}
     </>
-
   );
 }
